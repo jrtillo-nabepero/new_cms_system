d1e53937cadd17de721a1eefe851ec27d507fcfd
Jay-R Tillo <tillojayr@gmail.com>
2024-07-11 15:14:16 +0800

    last sections of the course

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,4 +14,22 @@ class UserController extends Controller
 
         return view('admin.users.profile', ['user' => $user]);
     }
+
+    public function update(User $user){
+
+        $inputs = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['min:6', 'max:255', 'confirmed']
+        ]);
+
+        if(request('avatar')){
+            $inputs['path'] = request('avatar')->store('images');
+        }
+
+        // dd($inputs);
+        $user->update($inputs);
+
+        return back();
+    }
 }
--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -4,6 +4,49 @@
 
         <h1>User Profile for : {{$user->name}}</h1>
 
+        <form action="{{route('user.profile.update', $user)}}" method="post" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="row">
+                <div class="col-4">
+                    <img class="img-profile rounded-circle mb-3" width="100" height="100" src="{{asset('storage/'. $user->path)}}">
+                    <div class="mb-3">
+                        <label for="avatar" class="form-label">Profile Picture</label>
+                        <input class="form-control" type="file" id="avatar" name="avatar">
+                    </div>
+                    <div class="form-group">
+                        <label for="title">Name</label>
+                        <input type="text" name="name" class="form-control" id="name" aria-describedby="" value="{{$user->name}}">
+                        @error('name')
+                            <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" class="form-control" id="email" value="{{$user->email}}">
+                        @error('email')
+                            <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control" id="password">
+                        @error('password')
+                            <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="password_confirmation">Confirm Password</label>
+                        <input type="password" name="password_confirmation" class="form-control" id="password_confirmation">
+                        @error('password')
+                            <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+
     @endsection
 
 </x-admin-master>
